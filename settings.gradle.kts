c715191d968f13a741fb8a032a43765709363276
DONXUX <ldu2175@gmail.com>
2022-11-29 14:33:27 +0900

    env: init

---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        gradlePluginPortal()
+        mavenCentral()
+    }
+}
+
+dependencyResolutionManagement {
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "ppong"
+include(":androidApp")
+include(":shared")
